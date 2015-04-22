ac838b089b23ecd56a6c2d9c31a2cf3be333b58b
Allan Hedelain <tristan202@gmail.com>
2015-04-23 07:16:10 +0800

    SystemUI: Add back status bar clock color option (1/2)
    Reference
    https://github.com/temasek/android_frameworks_base/commit/3eee47fd171042ac98eb58717ac0b935e4198218
    
    Modified for use on cm-13.0
    
    Change-Id: I1c48d99e0bb414f494f04efef03cb6a0299ed149

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -99,6 +99,9 @@ public class Clock extends TextView implements DemoMode {
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.STATUS_BAR_DATE_FORMAT), false,
                     this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUSBAR_CLOCK_COLOR), false,
+                    this, UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -336,6 +339,17 @@ public class Clock extends TextView implements DemoMode {
         mClockDateStyle = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUS_BAR_DATE_STYLE, CLOCK_DATE_STYLE_REGULAR,
                 UserHandle.USER_CURRENT);
+
+        int defaultColor = mContext.getResources().getColor(R.color.status_bar_clock_color);
+        int clockColor = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUSBAR_CLOCK_COLOR, defaultColor,
+                UserHandle.USER_CURRENT);
+        if (clockColor == Integer.MIN_VALUE) {
+            // flag to reset the color
+            clockColor = defaultColor;
+        }
+
+        setTextColor(clockColor);
         updateClock();
 		updateShowSeconds();
     }
