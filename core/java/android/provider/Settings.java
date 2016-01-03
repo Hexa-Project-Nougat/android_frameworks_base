d70a4d61c826db12f043c5d63c5cad195e4a8b26
LorDClockaN <davor@losinj.com>
2016-01-03 20:00:11 +0100

    FWB: Add clock/date in recents while in fullscreen mode (1/2)
    Change-Id: I9ecc5890269997453056dc3a41b583022db7bd6a
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5333,6 +5333,18 @@ public final class Settings {
          */
         public static final String CONFIRM_SHUTDOWN_SWITCH = "confirm_shutdown_switch";
 
+	/**
+         * Whether to display clock while recents are in full screen
+         * @hide
+         */
+        public static final String RECENTS_FULL_SCREEN_CLOCK = "recents_full_screen_clock";
+
+        /**
+         * Whether to display clock while recents are in full screen
+         * @hide
+         */
+        public static final String RECENTS_FULL_SCREEN_DATE = "recents_full_screen_date";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
