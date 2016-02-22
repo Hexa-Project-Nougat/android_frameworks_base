91a83b197bc6e8874ff02169b1e0f81ccd12f910
Alex Cruz <mazdarider23@gmail.com>
2016-02-22 09:06:57 +0530

    More Lockscreen Colors [1/2]
    --Next Alarm
    --Owner Info
    
    Change-Id: Iaf34def837f095615e10f2e9b4df5080b28d9419
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4962,6 +4962,20 @@ public final class Settings {
          * @hide
          */
         public static final String LOCKSCREEN_CLOCK_DATE_COLOR = "lockscreen_clock_date_color";
+		
+        /**
+         * Change the color of the lockscreen owner's info text
+         *
+         * @hide
+         */
+        public static final String LOCKSCREEN_OWNER_INFO_COLOR = "lockscreen_owner_info_color";
+
+        /**
+         * Change the color of the lockscreen alarm text
+         *
+         * @hide
+         */
+        public static final String LOCKSCREEN_ALARM_COLOR = "lockscreen_alarm_color";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
