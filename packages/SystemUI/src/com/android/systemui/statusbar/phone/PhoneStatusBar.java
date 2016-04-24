766f37f279cd55669744b8ecfc96dee59523b99c
Varun Date <date.varun123@gmail.com>
2016-04-24 13:08:35 +0530

    FWB :SmartBar Button Tint [1/3]
    With Reference to Patch https://github.com/AICP/frameworks_base/commit/85d97f4ad3b4d8933f48d9f2c09da049c79a409a
    
    thanks to lichti1901 for this initial Patch
    
    Patchset 2 : Fix Back Button Tinting
    Thanks to  LorDClockaN
    
    Based on this patch
    
    https://github.com/AICP/frameworks_base/commit/d09e1cc1295e2f21db8e7908f650e26f0d6bb2b6
    
    Patchset 3 : Fix New Actions FCs
    
    Change-Id: I2aa6be39616df8e2c823c92aee3f586ed3ed66ad
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -601,6 +601,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                    Settings.Secure.QS_COLUMNS),
                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NAVBAR_TINT_SWITCH),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NAVBAR_BUTTON_COLOR),
+                    false, this, UserHandle.USER_ALL);
              update();
          }
 		
@@ -615,6 +621,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
              } else if (uri.equals(Settings.Secure.getUriFor(
                      Settings.Secure.QS_COLUMNS))) {
                      updateResources();
+           }  else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.NAVBAR_TINT_SWITCH))) {
+                    mNavigationController.updateNavbarOverlay(mContext.getResources());
+           } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.NAVBAR_BUTTON_COLOR))) {
+                    mNavigationController.updateNavbarOverlay(mContext.getResources());
              }
              update();
  		}
