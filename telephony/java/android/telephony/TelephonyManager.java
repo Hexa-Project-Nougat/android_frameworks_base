5fd8c26a59ec8c9415f2f1be3d0749b9ecded831
Decad3nce <decad3nce@gmail.com>
2016-10-11 16:41:59 +0800

    QS: Add LTE tile [1/2]
    works only with 1 sim phone
    
    Change-Id: Ic6f6282c584ec28d12b07975707d95ab57935d89
    [neo: Device with dual sim can disable using overlay.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -1683,13 +1683,23 @@ public class TelephonyManager {
             return NETWORK_TYPE_UNKNOWN;
         }
     }
-
+    /**
+     * {@hide}
+     */
+    public void toggleLTE(boolean on) {
+        try {
+            getITelephony().toggleLTE(on);
+        } catch (RemoteException e) {
+            //Silently fail
+        }
+    }
     /**
      * Network Class Definitions.
      * Do not change this order, it is used for sorting during emergency calling in
      * {@link TelephonyConnectionService#getFirstPhoneForEmergencyCall()}. Any newer technologies
      * should be added after the current definitions.
      */
+
     /** Unknown network class. {@hide} */
     public static final int NETWORK_CLASS_UNKNOWN = 0;
     /** Class of broadly defined "2G" networks. {@hide} */
