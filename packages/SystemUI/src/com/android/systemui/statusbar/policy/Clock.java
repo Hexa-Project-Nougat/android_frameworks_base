f0655f364f2cad07003efd4094ef70240e7ad30a
KangDroid <hyunwoo286@gmail.com>
2016-11-13 18:05:53 +0900

    Fix build
    Change-Id: I1722bddaa1493ed9a387cc6dd8433795cbadad74

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -40,6 +40,7 @@ import android.view.Display;
 import android.widget.TextView;
 
 import com.android.systemui.DemoMode;
+import com.android.systemui.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
