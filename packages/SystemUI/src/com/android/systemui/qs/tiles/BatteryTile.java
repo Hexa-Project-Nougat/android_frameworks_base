267de978152f63129ec578ec8e50da089180bb8b
KangDroid <hyunwoo286@gmail.com>
2016-12-15 22:33:23 +0900

    Revert "BatteryTile: Clean up redundancy"
    This reverts commit d9e0af2e0ecdf907a294997db3e94bf6f4ace89d.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BatteryTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BatteryTile.java
@@ -59,14 +59,16 @@ public class BatteryTile extends QSTile<QSTile.State> implements BatteryControll
     private boolean mPluggedIn;
     private int mBatteryStyle;
 
+    public static final int BATTERY_STYLE_HIDDEN    = 4;
+    public static final int BATTERY_STYLE_TEXT      = 6;
+
     public BatteryTile(Host host) {
         super(host);
         mBatteryController = host.getBatteryController();
         mBatteryStyle = CMSettings.System.getInt(host.getContext().getContentResolver(),
-                CMSettings.System.STATUS_BAR_BATTERY_STYLE, BatteryMeterDrawable.BATTERY_STYLE_PORTRAIT);
-        if (mBatteryStyle == BatteryMeterDrawable.BATTERY_STYLE_HIDDEN || 
-                mBatteryStyle == BatteryMeterDrawable.BATTERY_STYLE_TEXT) {
-            mBatteryStyle = BatteryMeterDrawable.BATTERY_STYLE_PORTRAIT;
+                CMSettings.System.STATUS_BAR_BATTERY_STYLE, 0);
+        if (mBatteryStyle == BATTERY_STYLE_HIDDEN || mBatteryStyle == BATTERY_STYLE_TEXT) {
+            mBatteryStyle = 0;
         }
     }
 
@@ -126,10 +128,9 @@ public class BatteryTile extends QSTile<QSTile.State> implements BatteryControll
             @Override
             public Drawable getDrawable(Context context) {
                 mBatteryStyle = CMSettings.System.getInt(context.getContentResolver(),
-                        CMSettings.System.STATUS_BAR_BATTERY_STYLE, BatteryMeterDrawable.BATTERY_STYLE_PORTRAIT);
-                if (mBatteryStyle == BatteryMeterDrawable.BATTERY_STYLE_HIDDEN ||
-                        mBatteryStyle == BatteryMeterDrawable.BATTERY_STYLE_TEXT) {
-                    mBatteryStyle = BatteryMeterDrawable.BATTERY_STYLE_PORTRAIT;
+                        CMSettings.System.STATUS_BAR_BATTERY_STYLE, 0);
+                if (mBatteryStyle == BATTERY_STYLE_HIDDEN || mBatteryStyle == BATTERY_STYLE_TEXT) {
+                    mBatteryStyle = 0;
                 }
                 BatteryMeterDrawable drawable =
                         new BatteryMeterDrawable(context, new Handler(Looper.getMainLooper()),
