7b8b3b1f22868855b090362d81cad0f976221a80
Varun Date <date.varun123@gmail.com>
2017-04-03 09:48:22 +0530

    Switch to Enable or disable Vol up to disable overlays during boot [1/2]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8159,6 +8159,12 @@ public final class Settings {
         public static final String WAKE_GESTURE_ENABLED = "wake_gesture_enabled";
 
         /**
+         * Enable Volume Up to disable all overlays during boot.
+         * @hide
+         */
+        public static final String VOL_UP_DISABLE_OVERLAYS = "vol_up_disable_overlays";
+
+        /**
          * Whether the device should doze if configured.
          * @hide
          */
