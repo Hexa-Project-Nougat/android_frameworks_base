ecabb697a05c03672e651c559338e77f935fc33c
ezio84 <brabus84@gmail.com>
2017-04-06 19:36:47 +0200

    Ticker: allow to show music track info on new track being played [1/2]
    Change-Id: Ic14e06747ad2a5966ecf510cacd683476a80657d
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -56,6 +56,7 @@ import android.graphics.drawable.Icon;
 import android.hardware.SensorManager;
 import android.media.session.MediaController;
 import android.net.Uri;
+import android.media.MediaMetadata;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -2697,7 +2698,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected abstract void haltTicker();
     protected abstract void setAreThereNotifications();
     protected abstract void updateNotifications();
-    protected abstract void tick(StatusBarNotification n, boolean firstTime);
+    protected abstract void tick(StatusBarNotification n, boolean firstTime, boolean isMusic, MediaMetadata mediaMetaData);
     public abstract boolean shouldDisableNavbarGestures();
 
     public abstract void addNotification(StatusBarNotification notification,
@@ -2802,8 +2803,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         // Restart the ticker if it's still running
         if (updateTicker && isForCurrentUser) {
             haltTicker();
-            tick(notification, false);
-        }
+            tick(notification, false, false, null);
+          }
 
         setAreThereNotifications();
     }
