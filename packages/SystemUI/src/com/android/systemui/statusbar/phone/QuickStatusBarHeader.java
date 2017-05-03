726755aac909009b27238970d1b9330d372a7ab3
Varun Date <date.varun123@gmail.com>
2017-05-03 14:54:50 +0530

    Base:Dont do anything on weather errors for header & keyguard
    Change-Id: I21988d58d0559486897a2c395b88d931ca499c53
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
@@ -405,9 +405,7 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
 
     @Override
     public void weatherError(int errorReason) {
-        if (mWeatherData != null) {
-            setVisibility(View.GONE);
-        }
+        //do nothing
     }
 
     @Override
