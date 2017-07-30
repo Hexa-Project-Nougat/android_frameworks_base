a7c631b592aae6ee229e04c17db84bc4991d9323
KangDroid <hyunwoo286@gmail.com>
2017-07-30 23:57:55 +0900

    Revert "All tile scroller for quickbar settings [1/2]"
    This reverts commit 9722bac5e81fbdbd46bd834b70897e203e609924.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
@@ -42,7 +42,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -121,7 +120,6 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
     private float mDateTimeTranslation;
     private SparseBooleanArray mRoamingsBySubId = new SparseBooleanArray();
     private boolean mIsRoaming;
-    private HorizontalScrollView mQuickQsPanelScroller;
 
     private boolean isSettingsIcon;
     private boolean isSettingsExpanded;
@@ -174,8 +172,6 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
         mExpandIndicator = (ExpandableIndicator) findViewById(R.id.expand_indicator);
 
         mHeaderQsPanel = (QuickQSPanel) findViewById(R.id.quick_qs_panel);
-        mQuickQsPanelScroller = (HorizontalScrollView) findViewById(R.id.quick_qs_panel_scroll);
-        mQuickQsPanelScroller.setHorizontalScrollBarEnabled(false);
 
         mSettingsButton = (SettingsButton) findViewById(R.id.settings_button);
         mSettingsContainer = findViewById(R.id.settings_button_container);
@@ -666,11 +662,6 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
             Settings.System.QS_SETTINGS_EXPANDED_TOGGLE, 0) == 1;
     }
 
-    @Override
-    public void onClosingFinished() {
-        mQuickQsPanelScroller.scrollTo(0, 0);
-    }
-	
     public boolean isEditEnabled() {
         return Settings.System.getInt(mContext.getContentResolver(),
             Settings.System.QS_EDIT_TOGGLE, 1) == 1;
